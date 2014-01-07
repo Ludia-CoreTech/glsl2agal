7dbc76642271339deb53903e6148f552e826cca6
Kimmo Kotajärvi <kkotajarvi@ludia.com>
2014-01-07 10:18:42 -0500

    glsl2agalopt: Add usage help and use return instead of abort

--- swc/swc.cpp
+++ swc/swc.cpp
@@ -2,25 +2,25 @@
 Copyright (c) 2012, Adobe Systems Incorporated
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without 
+Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
 met:
 
-* Redistributions of source code must retain the above copyright notice, 
+* Redistributions of source code must retain the above copyright notice,
 this list of conditions and the following disclaimer.
 
 * Redistributions in binary form must reproduce the above copyright
-notice, this list of conditions and the following disclaimer in the 
+notice, this list of conditions and the following disclaimer in the
 documentation and/or other materials provided with the distribution.
 
-* Neither the name of Adobe Systems Incorporated nor the names of its 
-contributors may be used to endorse or promote products derived from 
+* Neither the name of Adobe Systems Incorporated nor the names of its
+contributors may be used to endorse or promote products derived from
 this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
 IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
 CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
 EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
 PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
@@ -98,6 +98,15 @@ static bool saveFile(const char* filename, const char* data)
    return true;
 }
 
+void printUsage()
+{
+   printf("Usage: glsl2agalopt -v|-f FILE [-e] [-optimize]\n");
+   printf("   -v: FILE is GLSL vertex shader\n");
+   printf("   -f: FILE is GLSL fragment shader\n");
+   printf("   -e: Shader is for GLES\n");
+   printf("   -optimize: run GLSL optimizer before conversion\n");
+}
+
 int
 main(int argc, char **argv)
 {
@@ -106,6 +115,12 @@ main(int argc, char **argv)
    bool vertexShader = false, freename = false, optimize = false, gles = false;
    const char* source = 0;
    char* dest = 0;
+
+   if (argc < 2)
+   {
+      printUsage();
+      return 1;
+   }
 
    for( int i=1; i < argc; i++ )
    {
@@ -121,6 +136,11 @@ main(int argc, char **argv)
             vertexShader = false;
          if( 0 == strcmp("-optimize", argv[i]) )
             optimize = true;
+         if( 0 == strcmp("-h", argv[i]) || 0 == strcmp("-help", argv[i]) )
+         {
+            printUsage();
+            return 1;
+         }
       }
       else
       {
@@ -132,7 +152,10 @@ main(int argc, char **argv)
    }
 
    if( !source )
-      abort();
+   {
+	  printf("No source specified\n");
+      return 1;
+   }
 
    if ( !dest ) {
       dest = (char *) calloc(strlen(source)+5, sizeof(char));
@@ -142,7 +165,7 @@ main(int argc, char **argv)
 
    const char* originalShader = loadFile(source);
    if( !originalShader )
-      abort();
+      return 1;
 
    AS3_DeclareVar(srcstr, String);
    AS3_CopyCStringToVar(srcstr, originalShader, strlen(originalShader));
@@ -156,7 +179,7 @@ main(int argc, char **argv)
    AS3_MallocString(optimizedShader, srcstr);
 
    if( !saveFile(dest, optimizedShader) )
-      abort();
+      return 1;
 
    return 0;
 
@@ -175,7 +198,6 @@ extern "C" void compileShader()
 
    bool gles = false;
    AS3_CopyScalarToVar(gles, gles);
-
 
    glslopt_ctx* gContext = glslopt_initialize(gles);
 
